b9ee8e01971eb2bff6bad660bd41942a5754f8c6
kirilllzaitsev <kzaitsev88@gmail.com>
2023-04-01 23:21:09 +0200

    feat(predictor): bound input size to 640x480

--- open_vocab_seg/utils/predictor.py
+++ open_vocab_seg/utils/predictor.py
@@ -1,17 +1,22 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 # Copyright (c) Meta Platforms, Inc. All Rights Reserved
 
+import logging
+
+import cv2
 import numpy as np
 import torch
-
+import yaml
 from detectron2.data import MetadataCatalog
 from detectron2.engine.defaults import DefaultPredictor
 from detectron2.utils.visualizer import ColorMode, Visualizer
 
 
 class OVSegPredictor(DefaultPredictor):
-    def __init__(self, cfg):
+    def __init__(self, cfg, custom_config_path="configs/3dv_project.yaml"):
         super().__init__(cfg)
+        self.custom_cfg = yaml.safe_load(open(custom_config_path))
+        self.input_img_size = (self.custom_cfg['INPUT']['WIDTH'], self.custom_cfg['INPUT']['HEIGHT'])
 
     def __call__(self, original_image, class_names):
         """
@@ -28,6 +33,7 @@ class OVSegPredictor(DefaultPredictor):
             if self.input_format == "RGB":
                 # whether the model expects BGR inputs or RGB
                 original_image = original_image[:, :, ::-1]
+            original_image = cv2.resize(original_image, self.input_img_size)
             height, width = original_image.shape[:2]
             image = self.aug.get_transform(original_image).apply_image(original_image)
             image = torch.as_tensor(image.astype("float32").transpose(2, 0, 1))
@@ -115,6 +121,9 @@ class VisualizationDemo(object):
         predictions = self.predictor(image, class_names)
         # Convert image from OpenCV BGR format to Matplotlib RGB format.
         image = image[:, :, ::-1]
+        if image.shape != self.predictor.input_img_size:
+            logging.warning(f"Original image size: {image.shape}, resized to {self.predictor.input_img_size}")
+            image = cv2.resize(image, self.predictor.input_img_size)
         visualizer = OVSegVisualizer(image, self.metadata, instance_mode=self.instance_mode, class_names=class_names)
         if "sem_seg" in predictions:
             r = predictions["sem_seg"]
